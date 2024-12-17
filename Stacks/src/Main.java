84f52c23c9fe036c368ae6621d55eb659a46854f
NebulaScout <ribak427@gmail.com>
2024-12-17 08:57:45 +0300

    Add stack implementation

---
+++ Stacks/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Stack myStack = new Stack(9);
+
+        myStack.getHeight();
+        myStack.getTop();
+
+        myStack.printStack();
+    }
+}
---
+++ Stacks/src/Stack.java
@@ -0,0 +1,53 @@
+/**
+ * This is a demonstration of stacks
+ * implemented using the pointer-based method.
+ *
+ * Stacks are visualised vertically thus we will only require one pointer to refer to the top node
+ * and extraction is done from top-to-bottom(LIFO), where the last item is extracted first until
+ * the last item in the stack is removed.
+ */
+
+public class Stack {
+    private Node top;
+    private int height;
+
+    /**
+     * Creates the structure of the node
+     */
+    class Node{
+        int value;
+        Node next;
+
+        Node(int value){
+            this.value = value;
+        }
+    }
+
+    /**
+     * Initialize a new node
+     * @param value the data contained in the node
+     */
+    public Stack(int value) {
+        Node newNode = new Node(value);
+        top = newNode;
+        height = 1;
+    }
+
+
+    public void printStack() {
+        Node temp = top;
+
+        while(temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void getTop() {
+        System.out.println("Top: " + top.value);
+    }
+
+    public void getHeight() {
+        System.out.println("Height: " + height);
+    }
+}
