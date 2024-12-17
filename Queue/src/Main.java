ae6217a6ce5388125109aef985420654b69c872d
NebulaScout <ribak427@gmail.com>
2024-12-17 10:29:39 +0300

    Implement queue data structure

---
+++ Queue/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Queue myQueue = new Queue(32);
+
+        myQueue.getFirst();
+        myQueue.getLast();
+        myQueue.getLength();
+
+        myQueue.printQueue();
+    }
+}
---
+++ Queue/src/Queue.java
@@ -0,0 +1,55 @@
+/**
+ * This is an implementation of the queue data structure
+ * <p>
+ * Queues follow the principle of FIFO where the first element that is inserted
+ * will be the first one to be removed.
+ */
+
+public class Queue {
+    private Node first;
+    private Node last;
+    private int length;
+
+    static class Node{
+        int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    /**
+     * Initialize a node
+     * @param value the data contained in the node
+     */
+    public Queue(int value) {
+        Node newNode = new Node(value);
+        first = newNode;
+        last = newNode;
+        length = 1;
+    }
+
+    public void printQueue() {
+        Node temp = first;
+
+        while(temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void getFirst() {
+        System.out.println("First: " + first.value);
+    }
+
+    public void getLast() {
+        System.out.println("Last: " + last.value);
+    }
+
+    public void getLength() {
+        System.out.println("Length: " + length);
+    }
+
+
+}
