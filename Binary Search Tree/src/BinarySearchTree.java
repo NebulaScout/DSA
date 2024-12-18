6db4c39c75f2a870715038e284cd65184f8caf3f
NebulaScout <ribak427@gmail.com>
2024-12-18 13:05:57 +0300

    Insert new siblings to the tree

--- Binary Search Tree/src/BinarySearchTree.java
+++ Binary Search Tree/src/BinarySearchTree.java
@@ -7,7 +7,7 @@ public class BinarySearchTree {
 
     Node root;
 
-    class Node{
+    static class Node{
         int value;
         Node left;
         Node right;
@@ -17,4 +17,31 @@ public class BinarySearchTree {
         }
     }
 
+    public boolean insert(int value) {
+        Node newNode = new Node(value);
+
+        if (root == null) {
+            root = newNode;
+            return true;
+        }
+
+        Node temp = root;
+        while (true) {
+            if (newNode.value == temp.value) return false;
+
+            if (newNode.value < temp.value) {
+                if (temp.left == null) {
+                    temp.left = newNode;
+                    return true;
+                }
+                temp = temp.left;
+            } else {
+                if (temp.right == null) {
+                    temp.right = newNode;
+                    return true;
+                }
+                temp = temp.right;
+            }
+        } // end of while loop
+    }
 }
